9a1db93a828b72dbaf33c7fe486a1b12a02091c0
User <bryant.t.tran1@gmail.com>
2017-06-06 19:36:11 -0700

    carousel quotes added

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $('.parallax-home-1').parallax({imageSrc: 'pictures/background.jpeg'});*/
 $('[data-parallax="scroll"]').each(function() {
     var $this = $(this);
     $this.parallax({imageSrc: $this.data('image-src') });
-}); 
+});
 
 //$('.parallax-home-2').parallax(); may need later
 
