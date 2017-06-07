3c65444f5a488b2657882ab9a1e2e428280ee5fe
User <YourName@Users-MacBook-Pro.local>
2017-06-06 19:20:08 -0700

    added quote carousel

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 $('.parallax-home-1').parallax({imageSrc: 'pictures/background.jpeg'});*/
 
 
-
+// Used to apply parallax scroll function data- etc
 $('[data-parallax="scroll"]').each(function() {
     var $this = $(this);
     $this.parallax({imageSrc: $this.data('image-src') });
