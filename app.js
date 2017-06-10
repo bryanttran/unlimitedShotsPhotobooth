892837046c14bc9285a6686f25f15608f7485d65
User <bryant.t.tran1@gmail.com>
2017-06-09 23:49:22 -0700

    angular magic

--- app.js
+++ app.js
@@ -34,7 +34,16 @@ USPApp.config(function($routeProvider) {
 
 // Controllers
 USPApp.controller('mainController', function($scope) {
-    $scope.message = 'Home';
+    
+    $scope.quote1 = {
+        message : "Ian and Joey, the owners, are very professional, accommodating, easy to deal with and the nicest too. Your guests will enjoy taking their pictures because Ian makes them feel comfortable and will cue them to do their best and wackiest faces. Try them and you will never regret it!",
+        name : "Nona Marie Olpindo Mejia"
+    };
+    
+    $scope.quote2 = {
+        message : "I recommend this photo booth to every occasion. Ian was so accommodating and very professional. All my guests were having fun and really enjoyed the unlimited shots. Thanks again and until next time.",
+        name : "Neth Lazatin"
+    };
 });
 
 USPApp.controller('aboutController', function($scope) {
